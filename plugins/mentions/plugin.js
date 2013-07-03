4318fabde3ee959db9a75cdb63a399e101ddf3d2
Albert Skibinski <albert@merge.nl>
2013-07-03 10:42:30 +0200

    Issue #2030269 by rudiedirkx: Fixed insert code replaces all realname instances, including existing

--- plugins/mentions/plugin.js
+++ plugins/mentions/plugin.js
@@ -11,7 +11,7 @@
 /*
  * Helper class needed to handle mentions.
  * This class is a singleton for each instance of CKEDITOR.
- * 
+ *
  * @param {Object} editor An instance of a CKEDITOR
  * @returns {null}
  */
@@ -19,15 +19,15 @@ function CKEDITOR_mentions (editor) {
   this.editor = editor;
   this.observe = 0;
   this.char_input = [];
-  
-  if ( CKEDITOR_mentions.caller != CKEDITOR_mentions.get_instance ) {  
-      throw new Error("This object cannot be instanciated");  
-  } 
+
+  if ( CKEDITOR_mentions.caller != CKEDITOR_mentions.get_instance ) {
+      throw new Error("This object cannot be instanciated");
+  }
 }
 
 /*
  * Collection of pairs editor id / instance of CKEDITOR_mentions
- * 
+ *
  * @type Array
  */
 CKEDITOR_mentions.instances = [];
@@ -35,7 +35,7 @@ CKEDITOR_mentions.instances = [];
 /*
  * Method used to get an instance of CKEDITOR_mentions linked to an instance of CKEDITOR.
  * Its design is based on the singleton design pattern.
- * 
+ *
  * @param {Object} editor An instance of a CKEDITOR
  * @returns An instance of CKEDITOR_mentions
  */
@@ -48,7 +48,7 @@ CKEDITOR_mentions.get_instance = function (editor) {
       return this.instances[i].instance;
     }
   }
-  
+
   // if no match was found, we add a row in our collection with the current CKEDITOR id and we instanciate CKEDITOR_mentions
   this.instances.push({
     id: editor.id,
@@ -60,7 +60,7 @@ CKEDITOR_mentions.get_instance = function (editor) {
 
 /*
  * This method delete the div containing the suggestions
- * 
+ *
  * @returns {null}
  */
 CKEDITOR_mentions.prototype.delete_tooltip = function () {
@@ -69,7 +69,7 @@ CKEDITOR_mentions.prototype.delete_tooltip = function () {
 
 /*
  * This method start the observation of the typed characters
- * 
+ *
  * @returns {null}
  */
 CKEDITOR_mentions.prototype.start_observing = function () {
@@ -78,7 +78,7 @@ CKEDITOR_mentions.prototype.start_observing = function () {
 
 /*
  * This method halts the observation of the typed characters and flush the properties used by CKEDITOR_mentions
- * 
+ *
  * @returns {null}
  */
 CKEDITOR_mentions.prototype.stop_observing = function () {
@@ -89,38 +89,75 @@ CKEDITOR_mentions.prototype.stop_observing = function () {
 
 /*
  * This methods send an ajax query to durpal ckeditor_mentions module and retrieve matching user.
- * 
+ *
  * @returns {null}
  */
-CKEDITOR_mentions.prototype.get_people = function () {
+CKEDITOR_mentions.prototype.get_people = function (selection) {
   //if less than 3 char are input (including @) we don't try to get people
-  if (this.char_input.length < 3) {
+
+  var str = this.char_input.join('');
+
+  if (str.length < 3) {
     this.delete_tooltip();
     return;
   }
-  
-  // make a copy of the array instead of a reference
-  var clone = this.char_input.slice(0);
-  str = clone.join('');
 
-  var element_settings = {};
+  var $ = jQuery;
 
-  element_settings.progress = { 'type': 'none' }; 
-  element_settings.url = '/ckeditor/mentions';
+  var editor = this.editor,
+    element_id = editor.element.getId();
 
-  var element = 'mentions_getpeople';
-  var base = 'mentions_getpeople';
-  element_settings.submit = {
-    'typed': str,
-    'element': this.editor.element.getId()
-  };
-  ajax = new Drupal.ajax(base, element, element_settings);
-  ajax.eventResponse(jQuery(ajax.element));
+  var range = selection.getRanges()[0],
+    startOffset = range.startOffset - str.length + 1,
+    element = range.startContainer.$;
+
+  $.get(Drupal.settings.basePath + 'ckeditor/mentions', {typed: str}, function(rsp) {
+
+    var ckel = $('#' + element_id);
+    var par = ckel.parent();
+
+    $('.mention-suggestions').remove();
+
+    if (rsp) {
+    	$('<div class="mention-suggestions">' + rsp.html + '</div>').insertAfter(par);
+  	}
+
+    $('.mention-users').click(function(e) {
+      e.preventDefault();
+
+      var mentions = CKEDITOR_mentions.get_instance(editor);
+      mentions.stop_observing();
+
+      // Shorten text node
+      element.textContent = element.textContent.substr(0, startOffset);
+
+      // Create link
+      var link = document.createElement('a');
+      link.href = Drupal.settings.basePath + 'user/' + $(this).data('uid');
+      link.textContent = '@' + $(this).data('realname');
+
+      // Insert link after text node
+      if ( element.nextSibling ) {
+        element.parentNode.insertBefore(link, element.nextSibling);
+      }
+      else {
+        element.parentNode.appendChild(link);
+      }
+
+      editor.focus();
+
+      var range = editor.createRange(),
+        el = new CKEDITOR.dom.element(link.parentNode);
+      range.moveToElementEditablePosition(el, link.parentNode.textContent.length);
+      range.select();
+    });
+
+  });
 };
 
 /*
  * This method returns if a char should stop the observation.
- * 
+ *
  * @param {int} charcode A character key code
  * @returns {Boolean} Whether or not the char should stop the observation
  */
@@ -150,32 +187,34 @@ CKEDITOR_mentions.prototype.break_on = function (charcode) {
     icons: '',
     init: function(editor) {
       var mentions = CKEDITOR_mentions.get_instance(editor);
-      
+
       /* The only way (it seems) to get a reliable, cross-browser and platform return for which key was pressed,
        * is using the jquery which function onkeypress. On keydown or up returns different values!
        * see also: http://jsfiddle.net/SpYk3/NePCm/
        */
       editor.on('contentDom', function(e) {
-        var editable = editor.editable();	
+        var editable = editor.editable();
 
-        /* we need the keyup listener to detect things like backspace, 
+        /* we need the keyup listener to detect things like backspace,
          * which does not register on keypress... javascript is weird...
          */
         editable.attachListener(editable, 'keyup', function(evt) {
           //console.log("e.which = " + evt.data.$.which);
           if (evt.data.$.which === 8) { // 8 == backspace
             mentions.char_input.pop();
-            mentions.get_people(mentions.char_input, editor);
+            var selection = this.editor.getSelection();
+            mentions.get_people(selection);
           }
-          
+
           // things which should trigger a stop observing, like Enter, home, etc.
           if (mentions.break_on(evt.data.$.which)) {
             mentions.stop_observing();
           }
+
         });
 
         editable.attachListener(editable, 'keypress', function(evt) {
-          /* btw: keyIdentifier is webkit only. 
+          /* btw: keyIdentifier is webkit only.
            * console.log("keyIdentifier = " + evt.data.$.keyIdentifier);
            * console.log("keyCode = " + evt.data.$.keyCode);
            * console.log("e.which = " + evt.data.$.which);
@@ -191,12 +230,13 @@ CKEDITOR_mentions.prototype.break_on = function (charcode) {
              * OR detect another @ while we are already observing
              * OR the length is longer than 11
              */
-            if ((mentions.char_input.length > 0 && typed_char === '@') || mentions.char_input.length > 11)	{
+            if ((mentions.char_input.length > 0 && typed_char === '@') || mentions.char_input.length > 11)  {
               mentions.stop_observing();
-            } 
-
-            mentions.char_input.push(typed_char);		
-            mentions.get_people();
+            } else {
+           		mentions.char_input.push(typed_char);
+            	var selection = this.editor.getSelection();
+            	mentions.get_people(selection);
+          	}
           }
         });
       }); // end editor.on
